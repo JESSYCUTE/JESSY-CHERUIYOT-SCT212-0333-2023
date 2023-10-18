f86c979646c90a5def8a07a09a5f16bed0acef34
JESSYCUTE <148341799+JESSYCUTE@users.noreply.github.com>
2023-10-18 19:05:23 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,33 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-
-    float a, b, celsius, fahrenheit;
-    int c;
-    printf("Press 1 For Convert Fahrenheit To Celsius\n");
-    printf("Press 2 For Convert Celsius To Fahrenheit\n");
-    printf("\nEnter Your Choice: ");
-
-    scanf("%d", & c);
-
-    switch (c) {
-    case 1:
-        printf("\nEnter The Temperature in Fahrenheit: ");
-        scanf("%f", & a);
-        celsius = 5 * (a - 32) / 9;
-        printf("\n\nCelsius Temperature is: %f ", celsius);
-        break;
-    case 2:
-        printf("\nEnter The Temperature in Celsius: ");
-        scanf("%f", & b);
-        fahrenheit = ((9 * b) / 5) + 32;
-        printf("\n\nFahrenheit Temperature is: %f ", fahrenheit);
-        break;
-    default:
-        printf("\n\nWrong Choice.....Try Again!!!\n");
-    }
-    return (0);
-
+int main(void)
+{int a, b, c;
+a = 5;
+b = -7;
+c = a - b;
+    printf("%d - %d = %d\n", a, b, c);
+    return 0;
 }
